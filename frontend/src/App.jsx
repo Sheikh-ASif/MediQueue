bd1897fe9d15aee73de6430517afcfc16369861e
Sheikh-ASif <mdasif74083@gmail.com>
2025-08-29 17:17:50 +0530

    Navbar+Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Home from './pages/Home.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Docters from './pages/Docters.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Login from './pages/Login.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Navbar from './components/Navbar.jsx'
+
 
 const App = () => {
   return (
-    <div className='text-3xl text-yellow-400 text-center font-bold'>MediQueue</div>
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/docters' element={<Docters/>}/>
+        <Route path='/docters/:speciality' element={<Docters/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointments'element={<MyAppointments/>}/>
+        <Route path='/appointments/:docId'element={<Appointment/>}/>
+
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Docters.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Docters = () => {
+  return (
+    <div>
+        Docter Pages
+    </div>
+  )
+}
+
+export default Docters
