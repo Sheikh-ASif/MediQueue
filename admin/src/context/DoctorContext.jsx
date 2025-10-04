48ae2ef4f54d76a83bd859f32c8f402de0d1423d
Sheikh-ASif <mdasif74083@gmail.com>
2025-10-04 21:33:12 +0530

    settings up the routes and doctor dashboard

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { createContext } from "react";
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 export const DoctorContext = createContext();
 
@@ -8,9 +10,30 @@ const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [dtoken, setDtoken] = useState(localStorage.getItem('dtoken')?localStorage.getItem('dtoken'):'')    
+    const [appointments, setAppointments] = useState([])
+
+    const getAppointments = async () => {
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/doctor/appointments', {headers:{dtoken}})
+            if (data.success) {
+                setAppointments(data.appointments.reverse())
+                console.log(data.appointments.reverse())
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
 
     const value = {
-        dtoken, setDtoken, backendUrl,
+        dtoken, setDtoken,
+        backendUrl, appointments,
+        setAppointments, getAppointments,
     }
 
     return(
