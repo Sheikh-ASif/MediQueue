6204bb736d5269ed64223dba998e820b1e57914a
Sheikh-ASif <mdasif74083@gmail.com>
2025-10-05 10:56:54 +0530

    solving the Logout Issues

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,15 +2,20 @@ import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import { useNavigate } from 'react-router-dom'
+import { DoctorContext } from '../context/DoctorContext'
 
 const Navbar = () => {
   const { atoken, setAtoken } = useContext(AdminContext)
+  const { dtoken, setDtoken } = useContext(DoctorContext)
+
   const navigate = useNavigate()
 
   const logout = () => {
     navigate('/')
     atoken && setAtoken('')
     atoken && localStorage.removeItem('atoken')
+    dtoken && setDtoken('')
+    dtoken && localStorage.removeItem('dtoken')
   }
 
   return (
