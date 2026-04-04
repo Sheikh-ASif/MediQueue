f439e14438c7d6264db73c1dcb4a3eff51491791
Sheikh-ASif <mdasif74083@gmail.com>
2026-04-04 10:47:35 +0530

    Removing the unneccessary comments

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,22 +33,3 @@ const Home = () => {
 
 export default Home;
 
-// import React from 'react'
-// import Header from '../components/Header'
-// import SpecialityMenu from '../components/specialityMenu'
-// import TopDocters from '../components/TopDocters'
-// import Banner from '../components/Banner'
-// // import Appointment from './Appointment'
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <SpecialityMenu/>
-//       <TopDocters/>
-//       <Banner/>
-//     </div>
-//   )
-// }
-
-// export default Home
